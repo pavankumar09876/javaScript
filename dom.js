36a1fa76a55ab8ff77a14e739eefd93fe4605cc0
pavanKumar09876 <pavankumar09876@users.noreply.github.com>
2025-11-25 12:15:01 +0530

    gitignore

--- dom.js
+++ dom.js
@@ -10,5 +10,25 @@
 // let c=document.querySelector(".second")
 // console.log(c)
 
-let d=document.querySelectorAll("h2")
-console.log(d)
+// let d=document.querySelectorAll("h2")
+// console.log(d)
+
+
+//// Dom properties to selet html elements
+// console.log(document)
+// console.log(document.title)
+// console.log(document.head)
+// console.log(document.body)
+// console.log(document.images)
+// console.log(document.scripts)
+
+//// Dom manipulications
+
+var add=document.createElement("h1")
+add.innerText="This add to js"
+console.log(add)
+var parent=document.getElementById("first")
+// parent.appendChild(add)
+console.log(parent.innerText)
+console.log(parent.innerHTML)
+parent.insertBefore(add,last)
